6e449b02c2e426a5dd7a2832fe86e7d37dadd9f2
Casper Banemann <casperbanemann@gmail.com>
2026-04-28 14:23:33 +0200

    add di styles, variables, fonts #152

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/tokens.scss'
-import './styles/theme.scss'
-import './styles/global.scss'
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
