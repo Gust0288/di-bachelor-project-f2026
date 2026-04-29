06cb1efc27cb7e511be01e4e5743425824537870
Casper Banemann <casperbanemann@gmail.com>
2026-04-29 09:55:52 +0200

    frontpage start setup #117

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 import { MyToastRegion } from './components/Toast/Toast'
 import WizardPage from './pages/Wizard/WizardPage'
 import LoginPage from './pages/Login/LoginPage'
+import styles from './App.module.scss'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/wizard" element={<WizardPage />} />
-      </Routes>
+      <div className={styles.app}>
+        <Header />
+        <main className={styles.main}>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/wizard" element={<WizardPage />} />
+          </Routes>
+        </main>
+      </div>
       <MyToastRegion />
     </BrowserRouter>
   )
--- frontend/src/pages/Login/LoginPage.tsx
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -4,6 +4,8 @@ export default function LoginPage() {
   return (
     <LoginLayout>
       <p>Login indhold her</p>
+      <p>Fortsæt uden bruger? - tryk her</p>
+      <p>Hvis logget ind med bruger - mulighed for autoudfyld personoplysninger</p>
     </LoginLayout>
   )
 }
